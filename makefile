86f92cc8f5ba94ad437e49326ac3810e7514a931
alessandrosoddu <alessandro.soddu@edu.unito.it>
2024-07-22 11:25:53 +0200

    Save initial state of the compressor

--- makefile
+++ makefile
@@ -1,13 +1,23 @@
 CXX = g++
 CXXFLAGS = -std=c++11 -Wall
 TARGET = read_audio
-SRCS = mp2_first.cpp
+SRCS = main.cpp
 
+# Directory di installazione di TwoLAME (modifica secondo la tua configurazione)
+TWOLAME_INCLUDE_DIR = /usr/include
+TWOLAME_LIB_DIR = /usr/lib
+
+# Librerie da collegare
+LIBS = -ltwolame
+
+# Regola per la compilazione
 $(TARGET): $(SRCS)
-	$(CXX) $(CXXFLAGS) -o $(TARGET) $(SRCS)
+	$(CXX) $(CXXFLAGS) -I$(TWOLAME_INCLUDE_DIR) -L$(TWOLAME_LIB_DIR) -o $(TARGET) $(SRCS) $(LIBS)
 
+# Regola per pulire i file generati
 clean:
 	rm -f $(TARGET) *.o
 
+# Regola per eseguire il programma
 run: $(TARGET)
 	./$(TARGET)
--- mp2_first.cpp
+++ mp2_first.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
-#include <twolame.h>
+#include <twolame.h>   
+#include "common.h"
+
 
 // 1. Frame Preparation
 
@@ -17,14 +19,60 @@ std::vector<short> readAudioFile(const std::string &filename) {
     return audioData;
 }
 
+// 1b. Save initial state of the compressor
+
+// 2. Watermarking functions
+
+// 3. Compressor state
+// 3a. Save the state before each iteration
+// 3b. Restore the state if necessary
+
 int main() {
+    twolame_options encoder; //twolame_options is type twolame_options_struct
+    encoder.samplerate_in = 44100;
+    encoder.samplerate_out = 44100;
+    encoder.num_channels_in = 2;
+    encoder.num_channels_out = 2;
+    //encoder.subband = new subband_t;
     
+    encoder.p0mem = new psycho_0_mem;
+    encoder.p1mem = new psycho_1_mem;
+    encoder.p2mem = new psycho_2_mem;
+    encoder.p3mem = new psycho_3_mem;
+    encoder.p4mem = new psycho_4_mem;
+
+    //std::vector<uint8_t> state;
+    //serialize(encoder, state);
+
+    twolame_options newEncoder;
+    newEncoder.subband = nullptr;
+    newEncoder.p0mem = nullptr;
+    newEncoder.p1mem = nullptr;
+    newEncoder.p2mem = nullptr;
+    newEncoder.p3mem = nullptr;
+    newEncoder.p4mem = nullptr;
+    //deserialize(state, newEncoder);
+
+    std::cout << "samplerate_in: " << newEncoder.samplerate_in << ", samplerate_out: " << newEncoder.samplerate_out << std::endl;
+
+    delete encoder.subband;
+    delete encoder.p0mem;
+    delete encoder.p1mem;
+    delete encoder.p2mem;
+    delete encoder.p3mem;
+    delete encoder.p4mem;
+    delete newEncoder.subband;
+    delete newEncoder.p0mem;
+    delete newEncoder.p1mem;
+    delete newEncoder.p2mem;
+    delete newEncoder.p3mem;
+    delete newEncoder.p4mem;
+
     std::string filename = "audio.wav";
     std::vector<short> audioData = readAudioFile(filename);
 
-    // Stampa alcuni dati del file audio
+    // Print some file audio data
     std::cout << "Il file audio contiene " << audioData.size() << " campioni." << std::endl;
-    std::cout << "I primi 10 campioni sono: ";
     for (size_t i = 0; i < 100 && i < audioData.size(); ++i) {
         std::cout << audioData[i] << " ";
     }
@@ -32,12 +80,3 @@ int main() {
 
     return 0;
 }
-
-// 1b. Load frame
-// 1c. Save initial state of the compressor
-
-// 2. Watermarking functions
-
-// 3. Compressor state
-// 3a. Save the state before each iteration
-// 3b. Restore the state if necessary

719d663e2ee241be75344cf55607129d03e94290
alessandrosoddu <alessandro.soddu@edu.unito.it>
2024-08-02 15:03:35 +0200

    adding logic for iteration

--- mp2_encoder.cpp
+++ mp2_encoder.cpp
@@ -36,7 +36,7 @@ void encoder(const char* input_file, const char* output_file){
 
     //Set parameters
     twolame_set_num_channels(options, CHANNELS);  // stereo
-    twolame_set_in_samplerate(options, IN_SAMPLERATE); // campionamento a 44.1kHz
+    twolame_set_in_samplerate(options, IN_SAMPLERATE); // sampling at 44.1kHz
     twolame_set_out_samplerate(options, OUT_SAMPLERATE);
     twolame_set_bitrate(options, 192);  // 192 kbps
     twolame_set_mode(options, TWOLAME_MONO);
@@ -81,8 +81,74 @@ void encoder(const char* input_file, const char* output_file){
 }
 
 //Encode ITERATION time a frame using the same state
-void frame_encoder(twolame_options state /*frame*/){
+void frame_encoder(const char* input_file, const char* output_file /*state options*/ /*frame*/){
 
+    // Open the input file
+    ifstream input(input_file, std::ios::binary);
+    if (!input) {
+        cerr << "Error: Unable to open the input file" << endl;
+        return;
+    }
+
+    // Initialize the encoder
+    twolame_options* options = twolame_init();
+
+    // Set parameters
+    twolame_set_num_channels(options, CHANNELS);  // stereo
+    twolame_set_in_samplerate(options, IN_SAMPLERATE); // sampling at 44.1kHz
+    twolame_set_out_samplerate(options, OUT_SAMPLERATE);
+    twolame_set_bitrate(options, BITRATE);  // 192 kbps
+    twolame_set_mode(options, TWOLAME_MONO);
+
+    // Initialize the encoder with parameters
+    if (twolame_init_params(options) != 0) {
+        cerr << "Error: Unable to initialize parameters" << endl;
+        twolame_close(&options);
+        return;
+    }
+
+    // Open the output file
+    ofstream output(output_file, std::ios::binary);
+    if (!output) {
+        cerr << "Error: Unable to open the output file" << endl;
+        twolame_close(&options);
+        return;
+    }
+
+    // Read and encode only one frame
+    short int pcm_input_buffer[BUFFER_SIZE * CHANNELS]; // PCM samples (interleaved for stereo)
+    unsigned char output_buffer[BUFFER_SIZE * CHANNELS];
+
+    // Read one frame from input file
+    while (input.read(reinterpret_cast<char*>(pcm_input_buffer), sizeof(pcm_input_buffer))) {
+
+        for(int i = 0; i < ITERATION; i++){
+            int num_samples = input.gcount() / sizeof(short int); // number of samples in the buffer
+            int num_bytes = twolame_encode_buffer_interleaved(
+                options, 
+                pcm_input_buffer, 
+                num_samples / CHANNELS, 
+                output_buffer, 
+                sizeof(output_buffer)
+            );
+
+            if (num_bytes < 0) {
+             cerr << "Error: Encoding error occurred" << endl;
+            } 
+            // Write to output file only in the last iteration
+            if (i == ITERATION - 1) {
+                output.write(reinterpret_cast<char*>(output_buffer), num_bytes);
+                cout << "Last iteration frame compressed and written to output file!" << endl;
+            }
+            else {
+                cout << "Single frame compressed and written to output file!" << endl;
+            }
+        }
+    } 
+    // Close the files and clean up
+    input.close();
+    output.close();
+    twolame_close(&options);
 }
 
 int main(int argc, char* argv[]){
@@ -93,6 +159,7 @@ int main(int argc, char* argv[]){
 
     const char* input_file = argv[1];
     const char* output_file = argv[2];
-    encoder(input_file, output_file);
+    //encoder(input_file, output_file);
+    frame_encoder(input_file, output_file);
 }
 
